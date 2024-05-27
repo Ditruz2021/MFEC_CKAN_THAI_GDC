624f79565ec09bf79eb2b85e2d1e912e8e89efdc
Krit Changloh <krit@ditruz.com>
2024-05-27 20:06:16 +0700

    Update feature prod

--- backup/ckan-docker/ckan/ckanext-thai-gdc/ckanext/thai_gdc/controllers/audit_log.py
+++ backup/ckan-docker/ckan/ckanext-thai-gdc/ckanext/thai_gdc/controllers/audit_log.py
@@ -1,8 +1,9 @@
 import ckan.plugins as p
-import ckan.lib.helpers as h
+import ckan.lib.helpers as helpers
 from ckan.plugins.toolkit import _
 import ckan.lib.uploader as uploader
 from ckanext.thai_gdc import helpers as thai_gdc_h
+from ckan.common import config
 
 from ckan.plugins.toolkit import (
     _, h, request,
@@ -11,21 +12,29 @@ from ckan.plugins.toolkit import (
 class AuditLogController(p.toolkit.BaseController):
 
     def index(self, data=None, errors=None, error_summary=None):
-        is_access = h.check_access('config_option_update')
-        if request.params:
-            q = request.params['q']
-        else:
-            q = ''
-        if not is_access:
-            p.toolkit.abort(404, _('Page Not Found'))
+        is_access = helpers.check_access('config_option_update')
+
+        limit = int(config.get(u'ckan.datasets_per_page', 20))
+        extra_vars = {}
+        extra_vars[u'page'] = helpers.Page(
+            collection=[],
+            items_per_page=limit
+        )
+        q = request.params.get(u'q', u'')
+        
+        
         try:
-            data = thai_gdc_h.get_audit_log_list(q)
+            extra_vars[u'page'].items = thai_gdc_h.get_audit_log_list(q)
+            extra_vars[u'page'].item_count = len(extra_vars[u'page'].items)
         except p.toolkit.ValidationError as e:
             errors = e.error_dict
             error_summary = e.error_summary
-            return self.index(data, errors, error_summary)
+            return self.index(extra_vars[u'page'].items, errors, error_summary)
         
         errors = errors or {}
         error_summary = error_summary or {}
-        extra_vars = {'data': data, 'errors': errors, 'error_summary': error_summary, 'count': len(data), 'q': q}
+        extra_vars[u'errors'] = errors 
+        extra_vars[u'error_summary'] = error_summary
+        extra_vars[u'count'] = len(extra_vars[u'page'].items)
+        extra_vars[u'q'] = q
         return p.toolkit.render('admin/audit_log.html', extra_vars=extra_vars)
